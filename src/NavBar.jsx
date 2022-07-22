e32351bf1a41cdf5fd74244c668716252e3eee5a
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-07-22 18:37:55 -0500

    update: nav

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,31 +7,31 @@ class NavBar extends Component {
     return (
       <React.Fragment>
 
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/#">eCommerce</a>
+    <a class="navbar-brand text-white" href="/#">eCommerce</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       <li class="nav-item">
-      <Link to="/" className="nav-link">
+      <Link to="/" className=" text-white nav-link">
                   Login
                 </Link>
         </li>
         <li class="nav-item">
-        <Link to="/dashboard" className="nav-link">
+        <Link to="/dashboard" className="text-white  nav-link">
                   Dashboard
                 </Link>
         </li>
         <li class="nav-item">
-        <Link to="/customers" className="nav-link">
+        <Link to="/customers" className="text-white nav-link">
                   Customers
                 </Link>
         </li>
         <li class="nav-item">
-        <Link to="/cart" className="nav-link">
+        <Link to="/cart" className=" text-white nav-link">
                   Shopping Cart
                   
                 </Link>
