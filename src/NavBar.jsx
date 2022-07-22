5e1f182656bff48f2c48f9d51cd4ff85187f2756
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-07-22 18:40:27 -0500

    update: res menu color

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -11,7 +11,7 @@ class NavBar extends Component {
   <div class="container-fluid">
     <a class="navbar-brand text-white" href="/#">eCommerce</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="bg-light navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
