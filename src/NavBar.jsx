d8ce36e933bb5ec313948fa230e22b42faeec202
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-07-22 18:31:47 -0500

    mobile-toggle

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,52 +6,43 @@ class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
-          <a className="navbar-brand" href="/#">
-            eCommerce
-          </a>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/#">eCommerce</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+      <Link to="/" className="nav-link">
                   Login
                 </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/dashboard" className="nav-link">
+        </li>
+        <li class="nav-item">
+        <Link to="/dashboard" className="nav-link">
                   Dashboard
                 </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/customers" className="nav-link">
+        </li>
+        <li class="nav-item">
+        <Link to="/customers" className="nav-link">
                   Customers
                 </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/cart" className="nav-link">
+        </li>
+        <li class="nav-item">
+        <Link to="/cart" className="nav-link">
                   Shopping Cart
                   
                 </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        </li>
+        
+       
+      </ul>
+      
+    </div>
+  </div>
+</nav>
       </React.Fragment>
     );
   }
